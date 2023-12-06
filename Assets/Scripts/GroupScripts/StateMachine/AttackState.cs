8f7e3600076ddf6d051cc6f65677a274fda3eec1
FIIZYLemon <matherdylan04@gmail.com>
2023-12-06 19:29:03 +0000

    Comments outlining exit states for each state

--- Assets/Scripts/GroupScripts/StateMachine/AttackState.cs
+++ Assets/Scripts/GroupScripts/StateMachine/AttackState.cs
@@ -14,7 +14,10 @@ namespace ZeroFour.StateMachine
 
         public override void ExitState()
         {
-
+            //is health or fuel critically low:
+            //Retreat
+            //has enemy left line of sight?:
+            //Chase
         }
 
         public override void UpdateState()
